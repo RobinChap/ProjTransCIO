16736997ab5b46dd9bccc8d084ae124b76284653
Auriles <auriles0404@gmail.com>
2016-05-11 10:55:03 +0200

    socket.io

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 app.get('/etage/:etagenum/chambre', function(req, res) {
-    res.render('chambre.ejs', {etage: req.params.etagenum});
+    res.render('gauge.jade', {etage: req.params.etagenum});
 });
 
 
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
-    res.send(404, 'Page introuvable !');
+    res.status(status).send(body)
 });
 
 app.listen(8080);
